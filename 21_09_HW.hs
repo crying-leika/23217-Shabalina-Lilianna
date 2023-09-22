080c3fde2d77ea07ea29a0c85453892d098f6979
leika <145035472+LeikaLia@users.noreply.github.com>
2023-09-22 18:38:29 +0700

    Update 21_09_HW.hs

--- 21_09_HW.hs
+++ 21_09_HW.hs
@@ -2,24 +2,34 @@
 even' :: Int -> Bool
 even' count' =  
     if count' `mod` 2 == 0 then  True else False
- 
 
 odd' :: Int -> Bool
 odd' count'' =  
     if count'' `mod` 2 == 0 then  False else True
 
+
 -- Посчитать N-е число Фибоначчи 
 computeFibb :: Int -> Int
 computeFibb 0 = 0
 computeFibb 1 = 1
-computeFibb n = computeFibb( n - 1 ) + computeFibb(n - 2)
+computeFibb n =
+    if n <= 0 then error "ancorrect" 
+    else computeFibb( n - 1 ) + computeFibb(n - 2)
+
+
+
 -- Посчитать сумму нечетых чисел Фибоначчи от 1 до N 
--- sumOddFibb :: Int -> Int
+sumOddFibbonacci :: Int -> Int
+sumOddFibbonacci 1 = 1
+sumOddFibbonacci n| n<=0 = error "ancorrect"
+                  | odd(computeFibb n) = (computeFibb n) + sumOddFibbonacci(n - 1)
+                  | otherwise = sumOddFibbonacci (n - 1)
 
 
-sumOddFibonacci :: Int -> Int
-sumOddFibonacci 0 = 0
-sumOddFibonacci 1 = 1
-sumOddFibonacci n =
-    if n `mod` 3 == 0 then n - 1 else n 
-sumOddFibonacci summ = computeFibb n + computeFibb (n - 1)
+
+-- магическое число
+sumMagicNumber :: Int -> Int
+
+sumMagicNumber n | n<=0 = error "ancorrect"
+                 | n < 10 = n
+                 | n > 10 = n `mod` 10 + sumMagicNumber(n `div` 10)
