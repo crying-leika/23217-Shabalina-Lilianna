8c3e25383b582ce73bd832d93056a0e9b6505ff3
leika <145035472+LeikaLia@users.noreply.github.com>
2023-09-28 18:26:24 +0700

    Add files via upload

---
+++ HW_2.hs
@@ -0,0 +1,28 @@
+
+-- 1 задача
+
+func_HW :: (Double -> Double) -> (Double -> Double) -> Double -> Double
+func_HW f g p | f(p) > g (p) = f(p)
+              | f(p) == g(p) = error "error"
+              | otherwise = g(p)
+
+-- 2 задача
+
+func_HW_2 :: (Double -> Double) -> Double -> Double
+func_HW_2 = func_HW exp 
+
+-- 3 задача 
+
+func_HW_3 :: (Double -> Double) -> Double -> Double -> Double 
+func_HW_3 f p n | n==0 = p
+                | n<0 = error "error"
+                | otherwise = func_HW_3 f (f p) (n-1)
+                
+
+                
+
+
+
+
+
+
